08ad5206e6849aef9e4e8772a3f74dc15d304fc9
Jian Huang <jhuangsoftware@gmail.com>
2015-12-17 12:48:51 -0500

    ADDED: template viewer and element usage

--- index.asp
+++ index.asp
@@ -9,46 +9,102 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" >
 	<title>Content Class Inspector</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link type="text/css" rel="stylesheet" href="css/shCoreDefault.css"/>
+	<link rel="stylesheet" href="css/custom.css" />
 	<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/handlebars-v2.0.0.js"></script>
 	<script type="text/javascript" src="js/content-class-inspector.js"></script>
 	<script type="text/javascript" src="rqlconnector/Rqlconnector.js"></script>
-
+	<script type="text/javascript" src="js/shCore.js"></script>
+	<script type="text/javascript" src="js/shBrushXml.js"></script>
+	
+	<script id="template-dialog" type="text/x-handlebars-template" data-container="#action-dialog" data-action="replace">
+		<div class="modal hide fade {{class}}" tabindex="-1" role="dialog" aria-hidden="true">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h3>{{name}}</h3>
+			</div>
+			<div class="modal-body">
+				<div class="alert">Loading...</div>
+			</div>
+		</div>
+	</script>
+	
+	<script id="template-dialog-template-variant-code" type="text/x-handlebars-template" data-container="#action-dialog .modal-body" data-action="replace">
+		<pre class="brush: html;">{{templatevariantcode}}</pre>
+	</script>
+	
+	<script id="template-dialog-template-variant-element-usage" type="text/x-handlebars-template" data-container="#action-dialog .modal-body" data-action="replace">
+		<table class="table">
+			<thead>
+				<tr>
+					<th>All Elements</th>
+					<th>Order of Elements in Code</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						<div class="alert">
+							{{#each contentclasselements}}
+							<div>{{name}}</div>
+							{{/each}}
+						</div>
+					</td>
+					<td>
+						<div class="alert alert-success">
+							{{#each templateelements}}
+							<div>{{name}}</div>
+							{{/each}}
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</script>
+	
 	<script id="template-content-class-folders" type="text/x-handlebars-template" data-container="#content-classes" data-action="replace">
 		<div class="accordion">
 			{{#each contentclassfolders}}
-			<div class="accordion-group">
+			<div class="accordion-group {{guid}}">
 				<div class="accordion-heading">
-					<span class="badge">{{count}}</span><a class="accordion-toggle" data-toggle="collapse" href="#{{guid}}">{{name}}</a>
+					<a class="accordion-toggle" data-toggle="collapse" href="#{{guid}}"><strong>{{name}}</strong></a>
 				</div>
 				<div id="{{guid}}" class="accordion-body collapse in">
-					<div class="alert">Loading...</div>
+					<div class="accordion-inner">
+						<div class="alert">Loading...</div>
+					</div>
 				</div>
 			</div>
 			{{/each}}
 		</div>
 	</script>
 	
-	<script id="template-content-classes" type="text/x-handlebars-template" data-container="#content-classes" data-action="replace">
+	<script id="template-content-classes-count" type="text/x-handlebars-template" data-container=".accordion-heading .accordion-toggle" data-action="append">
+		&nbsp;<span class="badge badge-info">{{count}}</span>
+	</script>
+	
+	<script id="template-content-classes" type="text/x-handlebars-template" data-container=".accordion-body .accordion-inner" data-action="replace">
 		{{#each contentclasses}}
-		<div class="alert alert-info {{guid}}">
+		<div class="alert alert-info {{guid}}" data-guid="{{guid}}">
 			<div>
-				<div class="btn"><i class="icon-search"></i></div> {{name}}
+				<div class="btn btn-link btn-copy" data-path="{{path}}"><strong>{{name}}</strong></div>
 			</div>
 			<div class="templates">
-
 			</div>
 		</div>
 		{{/each}}
 	</script>
 	
-	<script id="template-content-class-templates" type="text/x-handlebars-template" data-container="#content-classes" data-action="replace">
+	<script id="template-content-class-templates" type="text/x-handlebars-template" data-container=".templates" data-action="replace">
 		{{#each contentclasstemplates}}
-		<div>
-			<div class="btn"><i class="icon-th"></i></div>
-			<div class="btn btn-link" data-guid="{{guid}}">{{name}}</div>
-			<span class="label">{{projectvariantname}}</span>
+		<div class="{{guid}}">
+			<div class="btn btn-small btn-element-usage" data-guid="{{guid}}" data-path="{{path}}"><i class="icon-th"></i></div>
+			<div class="btn btn-link btn-template-variant-code" data-guid="{{guid}}" data-path="{{path}}">{{name}}</div>
+			{{#each projectvariantnames}}
+			<span class="label">{{name}}</span>
+			{{/each}}
 		</div>
 		{{/each}}
 	</script>
--- js/content-class-inspector.js
+++ js/content-class-inspector.js
@@ -2,12 +2,86 @@ function ContentClassInspector(RqlConnectorObj, ContentClassGuid) {
 	var ThisClass = this;
 	this.RqlConnectorObj = RqlConnectorObj;
 	
+	this.TemplateDialog = '#template-dialog';
+	this.TemplateDialogTemplateVariantCode = '#template-dialog-template-variant-code';
+	this.TemplateDialogTemplateVariantElementUsage = '#template-dialog-template-variant-element-usage';
 	this.TemplateContentClassFolders = '#template-content-class-folders';
+	this.TemplateContentClassesCount = '#template-content-classes-count';
 	this.TemplateContentClasses = '#template-content-classes';
 	this.TemplateContentClassTemplates = '#template-content-class-templates';
 	
 	this.ListAllProjectVariants(function(Data){
-		ThisClass.ListAllContentClassFolders()
+		var ProjectVariantGuids = Data;
+		
+		ThisClass.ListAllContentClassFolders(function(Data){
+			var ContentClassFolders = Data;
+			
+			$.each(ContentClassFolders, function(){
+				var ContentClassFolderGuid = this.guid;
+				var ContentClassFolderName = this.name;
+				
+				ThisClass.ListContentClasses(ContentClassFolderGuid, ContentClassFolderName, function(Data){
+					var ContentClasses = Data;
+
+					$.each(ContentClasses, function(){
+						var ContentClassGuid = this.guid;
+						var Path = this.path;
+
+						ThisClass.ListContentClassTemplates(Path, ContentClassGuid, ProjectVariantGuids);
+					});
+				});
+			});
+		});
+	});
+	
+	$('body').on('click', '.btn-copy', function(){
+		ThisClass.CopyToClipboard($(this).attr('data-path'));
+	});
+	
+	var ActionDialogContainer = $(ThisClass.TemplateDialog).attr('data-container');
+	
+	$(ActionDialogContainer).on('shown', '.modal', function(){
+		ThisClass.HideScrollbars();
+	});
+	
+	$(ActionDialogContainer).on('shown', '.template-variant-code.modal', function(){
+		var TotalHeight = $(this).height();
+		var HeaderHeight = $(this).find('.modal-header').height();
+		var ContentPadding = 50;
+		$(this).find('.modal-body').height(TotalHeight - HeaderHeight - ContentPadding);
+	});
+	
+	$(ActionDialogContainer).on('hidden', '.modal', function(){
+		ThisClass.ShowScrollbars();
+	});
+	
+	$('body').on('click', '.btn-template-variant-code', function(){
+		$(this).closest('.alert').removeClass('alert-info').addClass('alert-success');
+		
+		var ContentClassTemplateVariantGuid = $(this).attr('data-guid');
+		var ContentClassTemplatePath =  $(this).attr('data-path');
+		
+		ThisClass.UpdateArea(ThisClass.TemplateDialog, undefined, {'name': ContentClassTemplatePath, 'class': 'template-variant-code'});
+		
+		$ActionDialogModal = $(ActionDialogContainer).find('.modal');	
+		$ActionDialogModal.modal('show');
+		
+		ThisClass.ListContentClassTemplateVariantCode(ContentClassTemplateVariantGuid, function(Data){
+			SyntaxHighlighter.highlight();
+		});
+	});
+	
+	$('body').on('click', '.btn-element-usage', function(){
+		var ContentClassTemplateVariantGuid = $(this).attr('data-guid');
+		var ContentClassTemplatePath =  $(this).attr('data-path');
+		var ContentClassGuid = $(this).closest('.alert').attr('data-guid');
+		
+		ThisClass.UpdateArea(ThisClass.TemplateDialog, undefined, {'name': ContentClassTemplatePath, 'class': ''});
+		
+		$ActionDialogModal = $(ActionDialogContainer).find('.modal');	
+		$ActionDialogModal.modal('show');
+		
+		ThisClass.ListContentClassTemplateVariantElementUsage(ContentClassGuid, ContentClassTemplateVariantGuid);
 	});
 }
 
@@ -40,22 +114,21 @@ ContentClassInspector.prototype.ListAllContentClassFolders = function(CallbackFu
 		
 		$(Data).find('GROUP').each(function() {
 			var ContentClassFolder = {
-				'headline': $(this).attr('name'),
+				'name': $(this).attr('name'),
 				'guid': $(this).attr('guid')
-			}
-			;
+			};
+			
 			ContentClassFolders.push(ContentClassFolder);
 		});
 		
 		var ContentClassFoldersJson = {
-			'contentclassfolders': ContentClassFolders,
-			'count': ContentClassFolders.length
+			'contentclassfolders': ContentClassFolders
 		};
 		
 		ThisClass.UpdateArea(ThisClass.TemplateContentClassFolders, undefined, ContentClassFoldersJson);
 		
 		if(CallbackFunc){
-			CallbackFunc(ProjectVariantGuids);
+			CallbackFunc(ContentClassFolders);
 		}
 	});
 }
@@ -72,55 +145,204 @@ ContentClassInspector.prototype.ListContentClasses = function(ContentClassFolder
 		$(Data).find('TEMPLATE').each(function() {
 			var ContentClass = {
 				'name': $(this).attr('name'),
-				'guid': $(this).attr('guid')
-				'path': ContentClassFolderName + '/' + $(this).attr('name')
+				'guid': $(this).attr('guid'),
+				'path': ContentClassFolderName + ' / ' + $(this).attr('name')
 			};
 			
 			ContentClasses.push(ContentClass);
-			
-			ThisClass.UpdateArea(ThisClass.TemplateContentClasses, ' .' + ContentClassFolderGuid, ContentClassFoldersJson);
 		});
+
+		var ContentClassesJson = {
+			'contentclasses': ContentClasses
+		};
+
+		ThisClass.UpdateArea(ThisClass.TemplateContentClasses, '.' + ContentClassFolderGuid, ContentClassesJson);
+		
+		ThisClass.UpdateArea(ThisClass.TemplateContentClassesCount, '.' + ContentClassFolderGuid, {'count': ContentClasses.length});
 		
 		if(CallbackFunc){
-			CallbackFunc(ProjectVariantGuids);
+			CallbackFunc(ContentClasses);
 		}
 	});
 }
 
-ContentClassInspector.prototype.ListContentClassTemplates = function(ContentClassGuid, ProjectVariantGuids, CallbackFunc) {
+ContentClassInspector.prototype.ListContentClassTemplates = function(Path, ContentClassGuid, ProjectVariantGuids, CallbackFunc) {
+	var ThisClass = this;
+	
 	// list templates in a content class
-	var RqlXml = '<PROJECT><TEMPLATE guid="' + ContentClassGuid + '"><TEMPLATEVARIANTS action="list"/></TEMPLATE></PROJECT>';
+	var RqlXml = '<PROJECT><TEMPLATE guid="' + ContentClassGuid + '"><TEMPLATEVARIANTS action="list"/><TEMPLATEVARIANTS action="projectvariantslist" /></TEMPLATE></PROJECT>';
 
 	// send RQL XML
 	this.RqlConnectorObj.SendRql(RqlXml, false, function(Data) {
 		var ContentClassTemplates = [];
-		
-		$(data).find('TEMPLATEVARIANT').each(function() {
+
+		$(Data).find('TEMPLATEVARIANTS[action="list"] TEMPLATEVARIANT').each(function() {
 			var ContentClassTemplate = {
 				'name': $(this).attr('name'),
 				'guid': $(this).attr('guid'),
-				'projectvariantname': ProjectVariantGuids[$(this).attr("projectvariantguid")] 
+				'path': Path,
+				'projectvariantnames': [] 
 			};
+			
+			$(Data).find('TEMPLATEVARIANTS[action="projectvariantslist"] TEMPLATEVARIANT[guid="' + ContentClassTemplate.guid + '"]').each(function() {
+				var ProjectVariantName = {
+					'name': ProjectVariantGuids[$(this).attr('projectvariantguid')]
+				};
+				
+				ContentClassTemplate.projectvariantnames.push(ProjectVariantName);
+			});
+			
+			ContentClassTemplates.push(ContentClassTemplate);
 		});
-		
-		ThisClass.UpdateArea(ThisClass.TemplateContentClasses, ' .' + ContentClassGuid, ContentClassFoldersJson);
-		
+
+		var ContentClassTemplatesJson = {
+			'contentclasstemplates': ContentClassTemplates
+		};
+
+		ThisClass.UpdateArea(ThisClass.TemplateContentClassTemplates, '.' + ContentClassGuid, ContentClassTemplatesJson);
+
 		if(CallbackFunc){
 			CallbackFunc(ProjectVariantGuids);
 		}
 	});
 }
 
-ContentClassInspector.prototype.ListContentClassTemplateProjectVariantAssignment = function(ContentClassGuid, CallbackFunc) {
-	// list project variants assigned to template
-	var strRQL = padRQLXML('<PROJECT><TEMPLATE guid="' + ContentClassGuid + '" ><TEMPLATEVARIANTS action="projectvariantslist" /></TEMPLATE></PROJECT>');
-
+ContentClassInspector.prototype.ListContentClassTemplateVariantCode = function(ContentClassTemplateVariantGuid, CallbackFunc) {
+	var ThisClass = this;
+	
+	// list templates in a content class
+	var RqlXml = '<PROJECT><TEMPLATE><TEMPLATEVARIANT action="load" readonly="1" guid="' + ContentClassTemplateVariantGuid + '" /></TEMPLATE></PROJECT>';
+	
 	// send RQL XML
 	this.RqlConnectorObj.SendRql(RqlXml, false, function(Data) {
-		$(data).find('TEMPLATEVARIANT').each(function() {
-			$('#' + $(this).attr('guid') + ' span.projectvariants').append(ProjectVariantGuids[$(this).attr('projectvariantguid')] + ' ');
+		var TemplateVariantCode = $.trim($(Data).find('TEMPLATEVARIANT').text());
+		
+		ThisClass.UpdateArea(ThisClass.TemplateDialogTemplateVariantCode, undefined, {'templatevariantcode': TemplateVariantCode});
+		
+		if(CallbackFunc){
+			CallbackFunc(TemplateVariantCode);
+		}
+	});
+}
+
+
+ContentClassInspector.prototype.ListContentClassTemplateVariantElementUsage = function(ContentClassGuid, ContentClassTemplateVariantGuid, CallbackFunc) {
+	var ThisClass = this;
+	
+	this.LoadElementsInContentClass(ContentClassGuid, function(Data){
+		var ContentClassElements = Data;
+		var ContentClassElementsSorted = [];
+		
+		ThisClass.LoadElementsInContentClassTemplateVariant(ContentClassTemplateVariantGuid, function(Data){
+			var ContentClassTemplateVariantElements = Data;
+			
+			$.each(ContentClassTemplateVariantElements, function(){
+				var ContentClassTemplateVariantElement = this;
+				var Found = false;
+
+				$.each(ContentClassElements, function(){
+					var ContentClassElement = this;
+
+					if(ContentClassTemplateVariantElement.name == ContentClassElement.name ){
+						Found = true;
+					}
+				});
+
+				if(Found){
+					ContentClassElementsSorted.push(ContentClassTemplateVariantElement);
+				}
+			});
+			
+			$.each(ContentClassElements, function(){
+				var ContentClassElement = this;
+				var Found = false;
+				
+				$.each(ContentClassElementsSorted, function(){
+					var ContentClassElementSorted = this;
+					
+					if(ContentClassElementSorted.name == ContentClassElement.name ){
+						Found = true;
+					}
+				});
+				
+				if(!Found){
+					ContentClassElementsSorted.push(ContentClassElement);
+				}
+			});
+			
+			var ContentClassElementsJson = {
+				'contentclasselements': ContentClassElementsSorted,
+				'templateelements': ContentClassTemplateVariantElements
+			}
+			
+			ThisClass.UpdateArea(ThisClass.TemplateDialogTemplateVariantElementUsage, undefined, ContentClassElementsJson);
 		});
 	});
+}
+
+
+ContentClassInspector.prototype.LoadElementsInContentClass = function(ContentClassGuid, CallbackFunc) {
+	var ThisClass = this;
+	
+	var RqlXml = '<PROJECT><TEMPLATE action="load" guid="' + ContentClassGuid + '"><ELEMENTS childnodesasattributes="0" action="load"/><TEMPLATEVARIANTS action="list"/></TEMPLATE></PROJECT>';
+	this.RqlConnectorObj.SendRql(RqlXml, false, function(data){
+		var Elements = [];
+
+		$(data).find('ELEMENT').each(function(){
+			if($(this).attr('eltname')){
+				var ElementObj = {
+					guid: $(this).attr('guid'),
+					name: $(this).attr('eltname')
+				};
+				
+				Elements.push(ElementObj);
+			}
+		});
+		
+		if(CallbackFunc){
+			CallbackFunc(Elements);
+		}
+	});
+}
+
+ContentClassInspector.prototype.LoadElementsInContentClassTemplateVariant = function(ContentClassTemplateVariantGuid, CallbackFunc) {
+	var ThisClass = this;
+	
+	var RqlXml = '<PROJECT><TEMPLATE><TEMPLATEVARIANT action="load" readonly="1" guid="' + ContentClassTemplateVariantGuid + '" /></TEMPLATE></PROJECT>';
+	this.RqlConnectorObj.SendRql(RqlXml, false, function(data){
+		var Elements = [];
+		var ElementNames = [];
+		var TemplateRegexp = new RegExp('<' + '%([_\-a-zA-Z0-9]+?)%' + '>', 'ig');
+		var Match = null;
+		
+		while(Match = TemplateRegexp.exec($(data).text()))
+		{
+			ElementName = Match[1];
+			
+			if($.inArray(ElementName, ElementNames) == -1)
+			{
+				ElementNames.push(ElementName);
+			
+				var ElementObj = {
+					name: ElementName
+				};
+				
+				Elements.push(ElementObj);
+			}
+		}
+
+		if(CallbackFunc){
+			CallbackFunc(Elements);
+		}
+	});
+}
+
+ContentClassInspector.prototype.ShowScrollbars = function() {
+	$('body').css('overflow', 'auto');
+}
+
+ContentClassInspector.prototype.HideScrollbars = function() {
+	$('body').css('overflow', 'hidden');
 }
 
 ContentClassInspector.prototype.CopyToClipboard = function(Text) {
@@ -130,7 +352,7 @@ ContentClassInspector.prototype.CopyToClipboard = function(Text) {
 ContentClassInspector.prototype.UpdateArea = function(TemplateId, DataContainerAdditional, Data){
 	var ContainerId = $(TemplateId).attr('data-container');
 	if(DataContainerAdditional) {
-		ContainerId += DataContainerAdditional;
+		ContainerId = DataContainerAdditional + ' ' + ContainerId;
 	}
 	var TemplateAction = $(TemplateId).attr('data-action');
 	var Template = Handlebars.compile($(TemplateId).html());
